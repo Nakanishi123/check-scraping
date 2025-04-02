1bbb0c54673cb560eb18bb898a60aff9a71aa462
Nakanishi <contact114514@nakanishi.dev>
2025-04-02 14:15:44 +0900

    github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/check-scraping",
+  assetPrefix: "/check-scraping/",
 };
 
 export default nextConfig;
