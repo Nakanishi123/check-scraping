0b889f5ac9259bba432e65c1475356d8ab598365
Nakanishi <contact114514@nakanishi.dev>
2025-04-02 21:05:04 +0900

    navigator情報を状態管理し、コンポーネントのマウント後に表示するように変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,30 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [navigatorInfo, setNavigatorInfo] = useState({
+    language: "",
+    userAgent: "",
+  });
+  const [isMounted, setIsMounted] = useState(false);
+
   useEffect(() => {
+    setNavigatorInfo({
+      language: window.navigator.language,
+      userAgent: window.navigator.userAgent,
+    });
     if (window.navigator.webdriver) {
       alert("Webdriverを検出しました");
     }
+    setIsMounted(true);
   }, []);
 
   return (
     <div className="App">
       <h1>WebDriver検出テスト</h1>
-      <p>現在の言語: {window.navigator.language}</p>
-      <p>UserAgent : {window.navigator.userAgent}</p>
+      <p>現在の言語: {isMounted ? navigatorInfo.language : "読み込み中…"}</p>
+      <p>UserAgent : {isMounted ? navigatorInfo.userAgent : "読み込み中…"}</p>
     </div>
   );
 }
